b4104a0443737edaf8bda23862b6cef6e58a3b27
LeonTillberg <leontm@live.se>
2023-05-06 20:00:13 +0200

    JS and animations complete
    Only filler details left. possibly convert to non JQuery

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 $(document).ready(function () {
-    $('.menu-toggler').on('click', function(){
+    //When clicking the burger menu it opens the navbar:
+    $('.menu-toggler').on('click', function () {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
     });
+    //When clicking an item in the navbar it closes itself:
+    $('.top-nav .nav-link').on('click', function () {
+        $('-menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+    //When pressing a nav-item page takes user to its location:
+    $('nav a[href*="#"]').on('click', function () {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 2000);
+    });
+    //Press button to come to the top of the page:
+    $('#up').on('click', function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 2000);
+    });
+    //Animate elements to fade into the site:
+    AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once: true
+    })
 });
